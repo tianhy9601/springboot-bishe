e27e5fcfde5d6716212a249ffaaee95b89e975a1
tianhy9601 <799424189@qq.com>
2019-06-02 22:37:47 +0800

    aa

--- src/main/java/com/example/springboot/service/impl/DownloadFileImpl.java
+++ src/main/java/com/example/springboot/service/impl/DownloadFileImpl.java
@@ -1,7 +1,11 @@
 package com.example.springboot.service.impl;
 
 import com.example.springboot.dao.FileStudentDao;
+import com.example.springboot.dao.StudentDao;
+import com.example.springboot.dao.TeacherDao;
 import com.example.springboot.entity.FileStudent;
+import com.example.springboot.entity.SysStudent;
+import com.example.springboot.entity.SysTeacher;
 import com.example.springboot.service.DownloadFile;
 import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +18,10 @@ import java.util.List;
 public class DownloadFileImpl implements DownloadFile {
     @Autowired
     FileStudentDao fileDown;
-
+    @Autowired
+    StudentDao studentDao;
+    @Autowired
+    TeacherDao teacherDao;
 
     @Override
     public List<FileStudent> download(Integer stuIds) {
@@ -23,11 +30,22 @@ public class DownloadFileImpl implements DownloadFile {
         return fileDown.select(fileStudent);
     }
 
+    //学生数据表格
     @Override
-    public List<FileStudent> download(Integer stuIds, int page, int limit) {
+    public List<FileStudent> download(String stuName,Integer stuIds, int page, int limit) {
         PageHelper.startPage(page,limit);
         FileStudent fileStudent=new FileStudent();
         fileStudent.setStuId(stuIds);
+        fileStudent.setStuName(stuName);
+        return fileDown.select(fileStudent);
+    }
+
+    //教师数据表格
+    @Override
+    public List<FileStudent> downloadsss(Integer teacherId, int page, int limit) {
+        PageHelper.startPage(page,limit);
+        FileStudent fileStudent=new FileStudent();
+        fileStudent.setTeacherId(teacherId);
         return fileDown.select(fileStudent);
     }
 
@@ -39,17 +57,72 @@ public class DownloadFileImpl implements DownloadFile {
     }
 
     @Override
+    public List<SysStudent> downloadStu(Integer teacherId, int page, int limit) {
+        SysStudent sysStudent=new SysStudent();
+        sysStudent.setTeacherId(teacherId);
+        return studentDao.select(sysStudent);
+    }
+
+    @Override
+    public int countStu(Integer teacherId) {
+        SysStudent sysStudent=new SysStudent();
+        sysStudent.setTeacherId(teacherId);
+        return studentDao.selectCount(sysStudent);
+    }
+
+    @Override
+    public List<SysTeacher> downloadTeac(int page, int limit) {
+        SysTeacher teacher=new SysTeacher();
+        return teacherDao.select(teacher);
+    }
+
+    @Override
+    public int countTeac() {
+        SysTeacher teacher=new SysTeacher();
+        return teacherDao.selectCount(teacher);
+    }
+
+    @Override
+    public List<SysStudent> downloadStus(Integer id, int page, int limit) {
+        SysStudent sysStudent=new SysStudent();
+        sysStudent.setId(id);
+        return studentDao.select(sysStudent);
+    }
+
+    @Override
+    public List<SysTeacher> downloadTea(Integer teacherId, int page, int limit) {
+        SysTeacher sysTeacher=new SysTeacher();
+        sysTeacher.setId(teacherId);
+        return teacherDao.select(sysTeacher);
+    }
+
+    @Override
+    public int countTea(Integer teacherId) {
+        SysTeacher sysTeacher=new SysTeacher();
+        sysTeacher.setId(teacherId);
+        return teacherDao.selectCount(sysTeacher);
+    }
+
+    @Override
+    public int countStus(Integer id) {
+        SysStudent sysStudent=new SysStudent();
+        sysStudent.setId(id);
+        return studentDao.selectCount(sysStudent);
+    }
+
+    @Override
     public int count(Integer stuIds) {
         FileStudent fileStudent=new FileStudent();
         fileStudent.setStuId(stuIds);
         return fileDown.selectCount(fileStudent);
     }
-    //教师
+    //分页总数
     public int counts(FileStudent fileStudents) {
-        FileStudent fileStudent=new FileStudent();
-        return fileDown.selectCount(fileStudent);
+        //FileStudent fileStudent=new FileStudent();
+        return fileDown.selectCount(fileStudents);
     }
 
+    //下载
     @Override
     public FileStudent getById(Integer id) {
         FileStudent fileStudent = new FileStudent();
@@ -65,6 +138,7 @@ public class DownloadFileImpl implements DownloadFile {
         fileDown.deleteByPrimaryKey(fileStudent);
     }
 
+    //审核
     @Override
     @Transactional
     public void updatess(Integer id) {
@@ -73,10 +147,24 @@ public class DownloadFileImpl implements DownloadFile {
         fileDown.updateByPrimaryKey(fileStudent);
     }
 
+    //编辑
     @Override
     @Transactional
     public FileStudent update(FileStudent fileStudents) {
          fileDown.updateByPrimaryKey(fileStudents);
         return fileStudents;
+    }
+
+    @Override
+    @Transactional
+    public SysStudent updateStu(SysStudent sysStudent) {
+        studentDao.updateByPrimaryKey(sysStudent);
+        return sysStudent;
+    }
+
+    @Override
+    public SysTeacher updateTea(SysTeacher sysTeacher) {
+        teacherDao.updateByPrimaryKey(sysTeacher);
+        return sysTeacher;
     }
 }

4fee5fc6a63a43ef328729c62e579ed7ac940be2
tianhy9601 <799424189@qq.com>
2019-06-02 22:34:03 +0800

    aa

---
+++ src/main/java/com/example/springboot/dao/TeacherDao.java
@@ -0,0 +1,7 @@
+package com.example.springboot.dao;
+
+import com.example.springboot.entity.SysTeacher;
+import tk.mybatis.mapper.common.BaseMapper;
+
+public interface TeacherDao extends BaseMapper<SysTeacher> {
+}

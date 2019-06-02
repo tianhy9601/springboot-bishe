d216cce7c42753d5b8eead61553c6d825fd06e76
tianhy9601 <799424189@qq.com>
2019-06-02 22:24:18 +0800

    aa

--- src/main/java/com/example/springboot/controller/LoginController.java
+++ src/main/java/com/example/springboot/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.entity.Message;
+import com.example.springboot.entity.SysUser;
+import com.example.springboot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -12,15 +15,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LoginController {
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping("/index")
     public String index(Model model){
         Message msg =  new Message("测试标题","测试内容","额外信息，只对管理员显示");
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("msg", msg);
-        if(auth.getName().equals("admin"))
-            return "index";//如果是客户登录
+        SysUser user = userService.findByUserName(auth.getName());
+        if(user.getIsTeacher()!=null && user.getIsTeacher().equals(1))
+            return "index";//如果是后台管理人员登录
+        else if (user.getIsTeacher()!=null && user.getIsTeacher().equals(2))
+            return "addTeacher";
         else
-            return "user";//如果是后台管理人员登录
+            return "user";//如果是客户登录
 //        return "index";
     }
     @RequestMapping("/admin")

739c6aa4279367856e6ea812fa6ad4273fa132ae
tianhy9601 <799424189@qq.com>
2019-06-02 22:29:56 +0800

    aa

--- src/main/java/com/example/springboot/controller/SelectController.java
+++ src/main/java/com/example/springboot/controller/SelectController.java
@@ -1,9 +1,7 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.dao.UserDao;
-import com.example.springboot.entity.FileStudent;
-import com.example.springboot.entity.LayuiResonse;
-import com.example.springboot.entity.SysUser;
+import com.example.springboot.entity.*;
 import com.example.springboot.service.DownloadFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,6 +21,8 @@ public class SelectController {
     @Autowired
     private UserDao userDao;
 
+
+    //学生数据表格
     @ResponseBody
     @GetMapping("/select")
     public LayuiResonse<FileStudent> download(@RequestParam(required = false, defaultValue = "1") Integer page,
@@ -31,19 +31,81 @@ public class SelectController {
         SysUser sysUser = userDao.findByUserName(principal.getUsername());
         int s=sysUser.getId();
 //        String stuIds=sysUser.getId();
-        List<FileStudent>  students =  downloadFiles.download(s,page,limit);
+        String sssss=sysUser.getRealname();
+        List<FileStudent>  students =  downloadFiles.download(sssss,s,page,limit);
         int size = downloadFiles.count(s);
         LayuiResonse<FileStudent> resonse = new LayuiResonse<FileStudent>("0","",size,students);
         return  resonse;
     }
 
+    //教师看学生的信息界面
+    @ResponseBody
+    @GetMapping("/selectStu")
+    public LayuiResonse<SysStudent> downloadStudent(@RequestParam(required = false, defaultValue = "1") Integer page,
+                                             @RequestParam(required = false, defaultValue = "10") Integer limit) {
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        SysUser sysUser = userDao.findByUserName(principal.getUsername());
+        int s=sysUser.getId();
+        List<SysStudent>  students =  downloadFiles.downloadStu(s,page,limit);
+        int size = downloadFiles.countStu(s);
+        LayuiResonse<SysStudent> resonse = new LayuiResonse<SysStudent>("0","",size,students);
+        return  resonse;
+    }
+
+    //教师看学生的信息界面
+    @ResponseBody
+    @GetMapping("/selectTeaXue")
+    public LayuiResonse<SysTeacher> downloadTeacherXue(@RequestParam(required = false, defaultValue = "1") Integer page,
+                                                    @RequestParam(required = false, defaultValue = "10") Integer limit) {
+
+        List<SysTeacher>  teachers =  downloadFiles.downloadTeac(page,limit);
+        int size = downloadFiles.countTeac();
+        LayuiResonse<SysTeacher> resonse = new LayuiResonse<SysTeacher>("0","",size,teachers);
+        return  resonse;
+    }
+
+    //学生的用户信息界面
+    @ResponseBody
+    @GetMapping("/selectStus")
+    public LayuiResonse<SysStudent> downloadStudents(@RequestParam(required = false, defaultValue = "1") Integer page,
+                                                    @RequestParam(required = false, defaultValue = "10") Integer limit) {
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        SysUser sysUser = userDao.findByUserName(principal.getUsername());
+        int s=sysUser.getId();
+        List<SysStudent>  students =  downloadFiles.downloadStus(s,page,limit);
+        int size = downloadFiles.countStus(s);
+        LayuiResonse<SysStudent> resonse = new LayuiResonse<SysStudent>("0","",size,students);
+        return  resonse;
+    }
+
+    //教师的用户信息界面
+    @ResponseBody
+    @GetMapping("/selectTea")
+    public LayuiResonse<SysTeacher> downloadTeacher(@RequestParam(required = false, defaultValue = "1") Integer page,
+                                                     @RequestParam(required = false, defaultValue = "10") Integer limit) {
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        SysUser sysUser = userDao.findByUserName(principal.getUsername());
+        int s=sysUser.getId();
+        List<SysTeacher>  teachers =  downloadFiles.downloadTea(s,page,limit);
+        int size = downloadFiles.countTea(s);
+        LayuiResonse<SysTeacher> resonse = new LayuiResonse<SysTeacher>("0","",size,teachers);
+        return  resonse;
+    }
+
+
+
+    //教师数据表格
     @ResponseBody
     @GetMapping("/selects")
     public LayuiResonse<FileStudent> downloads(@RequestParam(required = false, defaultValue = "1") Integer page,
                                               @RequestParam(required = false, defaultValue = "10") Integer limit) {
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        SysUser sysUser = userDao.findByUserName(principal.getUsername());
+        int ss=sysUser.getId();
         FileStudent s=new FileStudent();
+        s.setTeacherId(sysUser.getId());
 //        String stuIds=sysUser.getId();
-        List<FileStudent>  students =  downloadFiles.downloads(page,limit);
+        List<FileStudent>  students =  downloadFiles.downloadsss(ss,page,limit);
         int size = downloadFiles.counts(s);
         LayuiResonse<FileStudent> resonse = new LayuiResonse<FileStudent>("0","",size,students);
         return  resonse;
@@ -61,6 +123,18 @@ public class SelectController {
     public FileStudent edits(@PathVariable(name = "id") Integer id, @RequestBody FileStudent ss) {
         ss.setId(id);
         return downloadFiles.update(ss);
+    }
+
+    @PutMapping("/editStu/{id}")
+    public SysStudent editStu(@PathVariable(name = "id") Integer id, @RequestBody SysStudent ss) {
+        ss.setId(id);
+        return downloadFiles.updateStu(ss);
+    }
+
+    @PutMapping("/editTea/{id}")
+    public SysTeacher editTea(@PathVariable(name = "id") Integer id, @RequestBody SysTeacher st) {
+        st.setId(id);
+        return downloadFiles.updateTea(st);
     }
 
     @GetMapping("/download/{id}")

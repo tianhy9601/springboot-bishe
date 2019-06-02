a2c6598a0dbbfce26cfc6ea40275082dbb5060dc
tianhy9601 <799424189@qq.com>
2019-06-02 22:33:28 +0800

    aa

---
+++ src/main/java/com/example/springboot/dao/StudentDao.java
@@ -0,0 +1,7 @@
+package com.example.springboot.dao;
+
+import com.example.springboot.entity.SysStudent;
+import tk.mybatis.mapper.common.BaseMapper;
+
+public interface StudentDao extends BaseMapper<SysStudent> {
+}
